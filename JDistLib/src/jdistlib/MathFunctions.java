4818d99120ab8ba162e9c14eb9eff76c4753a5ac
Roby <robbyjo@gmail.com>
2012-03-07 16:38:02 -0500

    add log1px

--- JDistLib/src/jdistlib/MathFunctions.java
+++ JDistLib/src/jdistlib/MathFunctions.java
@@ -529,6 +529,40 @@ public class MathFunctions {
 		}
 	}
 
+	/**
+	 * log1px takes a double and returns a double.
+	 * It is a Taylor series expansion of log(1+x).
+	 * x is presumed to be < 1.  As I have called it, x < .1,
+	 * and so I know the algorithm will terminate quickly.
+	 * The closer x is to 1, the slower this will be. (From AS 885)
+	 */
+	public static final double log1px(double x)
+	{
+		int n, sn;
+		double xn, ans, oans, term; //, eps;
+
+		if (!(abs(x) < 1.0e0)) {
+			return Double.NaN; //(0.0e0/0.0e0); /* NaN */
+		}
+		term = ans= oans = x;
+		oans = ans + (double) 1.0e0;
+		n = 1;
+		sn = 1;
+		xn = x;
+		/* Comparing ans!=oans is done here to insure that this calculation
+		continues until the accuracy of the machine is reached.  At some point,
+		the value is not being updated in successive iterations, that is time to
+		quit. */
+		while (ans != oans ) {
+			oans = ans;
+			sn *= -1;
+			xn *= x;
+			term= ((double)sn/(double)++n)*xn;
+			ans += term;
+		}
+		return(ans);
+	}
+
 	public static final double lgamma1p(double a)
 	{
 		final double eulers_const =	 0.5772156649015328606065120900824024;

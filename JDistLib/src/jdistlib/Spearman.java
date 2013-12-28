d1fc6a953229364bdfa6d14368bf5c30ced8deba
Roby <robbyjo@gmail.com>
2013-12-27 21:15:44 -0500

    update

--- JDistLib/src/jdistlib/Spearman.java
+++ JDistLib/src/jdistlib/Spearman.java
@@ -1234,14 +1234,14 @@ public class Spearman extends GenericDistribution {
 		s -= 2.0;
 		s = max(1, 2 * (((long) floor(s))/2)); // force s to be even, except when it is 
 		/*
-	if (CheckFriedmanExactF(r,n,X,&F,true,doRho)) {
+	if (CheckFriedmanExactF(r,n,X,&F,false,doRho)) {
 		return F;	 // Lower tail including X exactly
 	}
 		//*/
 		if (r > spearmanArray.length) {
 			double W = (s - 1.0) / (m + 2.0); // Corrected for continuity
 			double a = 0.5 * r - 1.0;
-			return Beta.cumulative(1.0 - W, a, a, true, false) - Beta.cumulative((1.0 - W) - 2.0, a, a, true, false);
+			return Beta.cumulative(1.0 - W, a, a, true, false) - Beta.cumulative((1.0 - W) - 2.0/(m+2), a, a, true, false);
 		}
 		int ss = (int) (0.5+((r*(r*r-1))/6.0)*(1.0+x));
 		if (r % 2 == 0) ss *= 4;

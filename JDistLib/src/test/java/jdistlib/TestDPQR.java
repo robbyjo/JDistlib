2070c9e030ae246e8bf2fcdb64663ff78a189416
Roby <robbyjo@gmail.com>
2013-12-31 18:29:50 -0500

    added tests for Gamma and Chi Square distributions

--- JDistLib/src/test/java/jdistlib/TestDPQR.java
+++ JDistLib/src/test/java/jdistlib/TestDPQR.java
@@ -18,8 +18,14 @@ package jdistlib;
 import jdistlib.rng.QMersenneTwister;
 import org.junit.Test;
 
-import static java.lang.Math.*;
-import static jdistlib.Constants.*;
+import static java.lang.Math.abs;
+import static java.lang.Math.exp;
+import static java.lang.Math.log;
+import static java.lang.Math.log1p;
+import static java.lang.Math.pow;
+import static jdistlib.MathFunctions.round;
+import static jdistlib.MathFunctions.gammafn;
+import static jdistlib.Constants.DBL_EPSILON;
 
 /**
  * Ported tests/d-p-q-r-tests.R plus some more.
@@ -44,6 +50,18 @@ public class TestDPQR {
 
 	static final boolean isEqual(double a, double b, double tol) {
 		return !Double.isNaN(a+b) && (a == b || abs(a - b) < tol);
+	}
+
+	static final void printBool(boolean b) {
+		System.out.println(b ? "[1] TRUE" : "[1] FALSE");
+	}
+
+	static final void printBool(boolean... b) {
+		if (b == null || b.length == 0) return;
+		System.out.print("[1]");
+		for (int i = 0; i < b.length; i++)
+			System.out.print(b[i] ? " TRUE" : " FALSE");
+		System.out.println();
 	}
 
 	@Test
@@ -105,8 +123,8 @@ public class TestDPQR {
 				}
 				prev_dg = dg;
 			}
-			System.out.println(success1 ? "[1] TRUE" : "[1] FALSE");
-			System.out.println(success2 ? "[1] TRUE" : "[1] FALSE");
+			printBool(success1);
+			printBool(success2);
 		}
 		return success;
 	}
@@ -128,7 +146,7 @@ public class TestDPQR {
 					success = cur_success = false;
 				}
 			}
-			System.out.println(cur_success ? "[1] TRUE" : "[1] FALSE");
+			printBool(cur_success);
 		}
 		return success;
 	}
@@ -150,16 +168,13 @@ public class TestDPQR {
 					success = cur_success = false;
 				}
 			}
-			System.out.println(cur_success ? "[1] TRUE" : "[1] FALSE");
+			printBool(cur_success);
 		}
 		System.out.println();
-		if (!isEqual(NegBinomial.cumulative(1, 0.9, 0.5, true, false), 0.777035760338812)
-			|| !isEqual(NegBinomial.cumulative(3, 0.9, 0.5, true, false), 0.946945347071519)) {
-			success = false;
-			System.out.println("[1] FALSE");
-		} else {
-			System.out.println("[1] TRUE");
-		}
+		boolean b = !isEqual(NegBinomial.cumulative(1, 0.9, 0.5, true, false), 0.777035760338812)
+				|| !isEqual(NegBinomial.cumulative(3, 0.9, 0.5, true, false), 0.946945347071519);
+		if (b) success = false;
+		printBool(!b);
 		return success;
 	}
 
@@ -176,7 +191,7 @@ public class TestDPQR {
 				success = cur_success = false;
 			}
 		}
-		System.out.println(cur_success ? "[1] TRUE" : "[1] FALSE");
+		printBool(cur_success);
 
 		cur_success = true;
 		for (int i = 0; i <= 5; i++) {
@@ -186,7 +201,7 @@ public class TestDPQR {
 				success = cur_success = false;
 			}
 		}
-		System.out.println(cur_success ? "[1] TRUE" : "[1] FALSE");
+		printBool(cur_success);
 
 		int n1 = 20, n2 = 16;
 		for (int i = 0; i < n1; i++) {
@@ -265,7 +280,75 @@ public class TestDPQR {
 				}
 			}
 		}
-		System.out.println(is_sym ? "[1] TRUE" : "[1] FALSE");
+		printBool(is_sym);
+		return success;
+	}
+
+	@Test
+	public static final boolean test_gamma() {
+		System.out.println("##__ Gamma Density (incl. central chi^2) __");
+		boolean success = true;
+		for (int i = 0; i < 100; i++) {
+			double x = round(Gamma.random(2, 1, random), 2);
+			for (int j = 0; j < 30; j++) {
+				double sh = round(LogNormal.random(0, 1, random), 2);
+				double Ga = gammafn(sh);
+				for (int k = 0; k < 30; k++) {
+					double sig = round(LogNormal.random(0, 1, random), 2);
+					double d1 = Gamma.density(x, sh, sig, false);
+					double d2 = Gamma.density(x/sig, sh, 1, false) / sig;
+					if (!isEqual(d1, d2)) {
+						System.err.println(String.format("Error: scaled dgamma = %g, manually scaled dgamma = %g", d1, d2));
+						success = false;
+					}
+					double d3 = 1.0 / (Ga * pow(sig, sh)) * pow(x, sh - 1.0) * exp(-x / sig);
+					if (!isEqual(d1, d3)) {
+						System.err.println(String.format("Error: scaled dgamma = %g, manually comp dgamma = %g", d1, d3));
+						success = false;
+					}
+				}
+			}
+		}
+
+		double Inf = Double.POSITIVE_INFINITY, xMax = Double.MAX_VALUE;
+		printBool(Gamma.cumulative(1, Inf, Inf, true, false) == 0);
+		printBool(Double.isNaN(Gamma.cumulative(Inf, 1, Inf, true, false))
+			&& Double.isNaN(Gamma.cumulative(Inf, Inf, Inf, true, false)));
+		double p = Gamma.cumulative(Inf, 1, xMax, true, false);
+		if (!isEqual(p, 1)) {
+			System.err.println(String.format("Error: pgamma(Inf, 1, xMax) = %g", p));
+			success = false;
+		}
+		p = Gamma.cumulative(xMax, 1, Inf, true, false);
+		if (!isEqual(p, 0)) {
+			System.err.println(String.format("Error: pgamma(xMax, 1, Inf) = %g", p));
+			success = false;
+		}
+
+		double[] scLrg = new double[] { 2, 100, 1e299, 1e300, 1e301, 1e302, 1e307, xMax, Inf };
+		// Supplied values by R authors seem to be lacking precision
+		//double[] ans = new double[] {0, 0, -0.000499523968713701, -1.33089326820406,
+		//		-5.36470502873211, -9.91015144019122, -32.9293385491433, -38.707517174609, Double.NEGATIVE_INFINITY
+		//};
+		double[] ans = new double[] {0, 0, -0.0004995239687137007075432, -1.330893268204054846748, -5.364705028732111635748,
+			-9.910151440191221183795, -32.9293385491432459844, -38.70751717460898788659, Double.NEGATIVE_INFINITY
+		};
+		for (int i = 0; i < scLrg.length; i++) {
+			p = Gamma.cumulative(1e300, 2, scLrg[i], true, true);
+			if (!isEqual(p, ans[i], 2e-15)) {
+				System.err.println(String.format("Error: pgamma(1e300, 2, %g) = %3.18g. Correct answer = %3.18g", scLrg[i], p, ans[i]));
+				success = false;
+			}
+		}
+
+		p = 7e-4; double df = 0.9, lim = 1e-15;
+		double[] d = new double[] {
+		abs(1 - ChiSquare.cumulative(ChiSquare.quantile(p, df, true, false), df, true, false) / p),
+		abs(1 - ChiSquare.cumulative(ChiSquare.quantile(1-p, df, false, false), df, false, false) / (1-p)),
+		abs(1 - ChiSquare.cumulative(ChiSquare.quantile(log(p), df, true, true), df, true, true) / log(p)),
+		abs(1 - ChiSquare.cumulative(ChiSquare.quantile(log1p(-p), df, false, true), df, false, true) / log1p(-p))
+		};
+		printBool(d[0] < lim, d[1] < lim, d[2] < lim, d[3] < lim);
 		return success;
 	}
 
@@ -277,5 +360,6 @@ public class TestDPQR {
 		test_poisson();
 		test_signrank();
 		test_wilcox();
+		test_gamma();
 	}
 }

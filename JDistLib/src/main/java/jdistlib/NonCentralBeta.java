9f875416676cf5bf4ae267f3e34c316692f28774
Roby <robbyjo@gmail.com>
2014-01-07 13:23:25 -0500

    Add computation of non-centrality parameter

--- JDistLib/src/main/java/jdistlib/NonCentralBeta.java
+++ JDistLib/src/main/java/jdistlib/NonCentralBeta.java
@@ -271,4 +271,117 @@ public class NonCentralBeta extends GenericDistribution {
 	public double random() {
 		return random(a, b, ncp, random);
 	}
+
+	// This is taken from Baharev and Kemeny
+	/*
+	2-moment central F approximation; Patnaik P. B. 1949.
+	The non-central chi-square and F distribution and their applications;
+	Biometrika; 36: 202-232.
+	*/
+	static final double patnaik2(double x, double nu1, double nu2, double lambda) {
+		return F.cumulative( x/(1+lambda/nu1), (nu1+lambda)*((nu1+lambda)/(nu1+2*lambda)), nu2, true, false);
+	}
+
+	/**
+	This function gives an initial value of lambda for the Newton iteration.
+	First, the lambda value is bracketed, then bisection is used to find a 
+	better approximation. This function uses the 2-moment central F 
+	approximation of Patnaik.
+	 */
+	static final double guess(double prob, double y, double nu1, double nu2) {
+		double x, lambdal, lambdam, lambdau, fl, fm, fu;
+		int itr_cnt;
+
+		/* FIXME: cancellation ? */
+		x = nu2*y/(nu1*(1.0-y));
+		lambdal = 0.0;
+		lambdau = 1.0;
+		fl = F.cumulative(x, nu1, nu2, true, false);
+		/* In this case there is no solution */
+		if (fl < prob)
+			throw new RuntimeException("no solution (most likely a bug)");
+
+		fu = patnaik2(x, nu1, nu2, lambdau);
+		/* Bracketing lambda: lambdal <= lambda <= lambdau */
+		for (itr_cnt=1; ((fl-prob)*(fu-prob)>0.0)&&itr_cnt<=17; ++itr_cnt) {
+			fl = fu;
+			lambdal = lambdau;
+			lambdau = 2.0*lambdau;
+			fu = patnaik2(x,nu1,nu2,lambdau);
+		}
+
+		if (itr_cnt == 18)
+			throw new RuntimeException("failed to bracket lambda, it is likely to be LARGE");
+
+		/* find a better approximation of lambda by bisection */
+		lambdam = (lambdal + lambdau)/2.0;
+		for (itr_cnt=1; (((lambdau-lambdal)>1.0e-4*lambdau)&&((lambdau-lambdal) > 0.001))&&(itr_cnt<=29); ++itr_cnt) {
+			fm = patnaik2(x, nu1, nu2, lambdam);
+			if ((fm-prob)*(fu-prob) < 0.0) {
+				fl = fm; lambdal = lambdam;
+			} else {
+				fu = fm; lambdau = lambdam;
+			}
+			lambdam = (lambdal + lambdau)/2.0;
+		}
+		if (itr_cnt == 30)
+			throw new RuntimeException("failed to find initial guess");
+		return lambdam;
+	}
+
+	/**
+	Given prob, x, a and b, this function returns the corresponding 
+	noncentrality parameter of the noncentral beta distribution.
+
+	I.e. the following equation
+
+	I_x(a, b, lambda) = prob
+
+	is solved for lambda with Newton iteration.
+
+	This function works just fine when supplied with meaningful input
+	data (and from practically meaningful range) but may easily crash
+	if not. Please be nice.
+	*/
+	public static final double calculate_ncp(double prob, double x, double a, double b) {
+		double ql, qu, c, d, p, lambda, lambda_new, k, f, g, mu, eps, eps2;
+		int itr_cnt;
+		lambda_new = guess(prob, x, 2.0*a, 2.0*b);
+
+		/* FIXME: are these tolerances OK ?  */
+		eps  = DBL_EPSILON; //1.0e-7;
+		eps2 = DBL_EPSILON; //1.0e-6;
+
+		itr_cnt = 0;
+
+		do {
+			lambda = lambda_new;
+			mu = lambda/2.0;
+			ql = Poisson.quantile(eps, mu, true, false);
+			qu = Poisson.quantile(eps, mu, false, false);
+			k = qu;
+			c = Beta.cumulative(x, a+k, b, true, false);
+			d = x*(1.0-x)/(a+k-1.0)*Beta.density(x, a+k-1, b, false);
+			p = Poisson.density(k, mu, false);
+			f=p*c;
+			p = k/mu*p;
+			g = p*d;
+			for (k = qu-1; k >= ql; --k) {
+				c=c+d;
+				d=(a+k)/(x*(a+k+b-1))*d;
+				f=f+p*c;
+				p=k/mu*p;
+				g=g+p*d;
+			}
+			/* Newton step */
+			lambda_new = lambda+2.0*(f-prob)/g;
+			if (lambda_new <= 0.0)
+				lambda_new = lambda/2.0;
+			++itr_cnt;
+		}
+		while ((abs(lambda_new-lambda) > eps2*lambda_new)&&(itr_cnt<=10));
+		if (itr_cnt == 11)
+			throw new RuntimeException("newton iteration failed");
+		return lambda_new;
+	}
 }
